9537ad97b0a784b01dd09cdf8f41bd3a9c099cf1
JorgeAzzuFranco <00074216@uca.edu.sv>
2018-04-05 16:17:54 -0600

    Solo presentacion

---
+++ app/src/main/java/com/tresemejota/ipcalculator/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tresemejota.ipcalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
